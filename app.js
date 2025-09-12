fe343feb19d26d9e7381bba732ce11d664806485
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-09-12 19:56:47 -0300

    Exercício 01

---
+++ app.js
@@ -0,0 +1,8 @@
+let nome = "", idade = 0
+nome = prompt("Informe seu nome: ")
+idade = prompt("Informe sua idade: ")
+alert("Olá, seja bem vindo(a) " + nome + " \nVocê tem " + idade + " anos")
+
+console.log("Olá, seja bem vindo(a) " + nome + " \nVocê tem " + idade + " anos")
+
+document.write("Olá, seja bem vindo(a) " + nome + " <br>Você tem " + idade + " anos")
