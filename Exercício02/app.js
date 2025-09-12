ec40624b941655a7356735dbc0e771f42d0c08fd
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-09-12 20:18:39 -0300

    Exercício02

---
+++ Exercício02/app.js
@@ -0,0 +1,18 @@
+let n1 = 10, n2 = 2
+console.log (n1+n2) // + operador de adição
+document.getElementById("resultado_soma").innerText = n1+n2
+ 
+console.log (n1-n2) // - operador de subtração
+document.getElementById("resultado_subtracao").innerText = n1-n2
+ 
+console.log (n1*n2) // * operador de multiplicação
+document.getElementById("resultado_multiplicacao").innerText = n1*n2
+ 
+console.log (n1/n2) // / operador de divisão
+document.getElementById("resultado_divisao").innerText = n1/n2
+ 
+console.log (n1%n2) // % operador de módulo, pega o resto da divisão
+document.getElementById("resultado_modulo").innerText = n1%n2
+ 
+console.log (n1**n2) // ** operador de exponenciação, pega um número e eleva a potência do outro 10²
+document.getElementById("resultado_exponenciacao").innerText = n1**n2
