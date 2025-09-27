6395e601d2f2926ae39225d2de3f49114b16a382
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-09-26 22:32:12 -0300

    Bonus001

---
+++ bonus001/app.js
@@ -0,0 +1,28 @@
+function fnTocarSom(tecla) {
+    if (tecla == 65) {
+        document.getElementById("som-65").currenTime = 0
+        document.getElementById("som-65").play()
+    } else if (tecla == 83) {
+        document.getElementById("som-83").currenTime = 0
+        document.getElementById("som-83").play()
+    } else if (tecla == 68) {
+        document.getElementById("som-68").currenTime = 0
+        document.getElementById("som-68").play()
+    } else if (tecla == 70) {
+        document.getElementById("som-70").currenTime = 0
+        document.getElementById("som-70").play()
+    } else if (tecla == 71) {
+        document.getElementById("som-71").currenTime = 0
+        document.getElementById("som-71").play()
+    } else if (tecla == 72) {
+        document.getElementById("som-72").currenTime = 0
+        document.getElementById("som-72").play()
+    } else if (tecla == 74) {
+        document.getElementById("som-74").currenTime = 0
+        document.getElementById("som-74").play()
+    } else if (tecla == 75) {
+        document.getElementById("som-75").currenTime = 0
+        document.getElementById("som-75").play()
+    }
+
+}
